1946c8210b06738b72315330c02eb0a7fd737a71
elmasbusenur <elmasbusenur@gmail.com>
2019-04-13 11:42:42 +0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void increment(int *);
+int main() {
+    int i;
+    i=5;
+    printf("oncesi %d\n",i);
+    increment(&i);
+    printf("sonrasi %d\n",i);
+    getchar();
+    return 0;
+}
+void increment(int *k)
+{
+    (*k)++;
+}
